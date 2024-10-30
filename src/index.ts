fe3407148158fabb144313564398dbe9a3ac9545
worksofliam <mrliamallan@live.co.uk>
2024-10-30 13:00:45 -0400

    Add validator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { json } from "body-parser";
+import * as OpenApiValidator from 'express-openapi-validator';
 
 import db, { connectionString } from "./db";
 import root from "./routes/root";
@@ -11,9 +12,25 @@ const port = process.env.PORT || 3000;
 
 app.use(json());
 
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: 'src/spec/spec.yaml',
+    validateRequests: true, // (default)
+    validateResponses: true, // false by default
+  }),
+);
+
 app.use(root);
 app.use('/employees', employees);
 app.use('/departments', departments);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  // format error
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
 
 db.connect(connectionString).then(() => {
   app.listen(port, () => {
